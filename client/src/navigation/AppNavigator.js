74c7ac7d9a452744e7b0f61451cbb64b242b5a8f
Shamlan Ahamed <ssahamed121@gmail.com>
2025-06-22 01:28:30 +0530

    backend and frontend files with test files connected to mongo and ran first login test and success

--- client/src/navigation/AppNavigator.js
+++ client/src/navigation/AppNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
@@ -7,12 +6,10 @@ import HomeScreen from '../screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
-  </NavigationContainer>
 );
 
 export default AppNavigator;
